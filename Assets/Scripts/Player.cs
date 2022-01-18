1f7e60acba52bad70fccd6a45a4636f908c88bd7
JemCopeCodes <thejemmeh@gmail.com>
2022-01-18 13:26:31 -0800

    Working game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	public Transform groundCheckTransform;
+	[SerializeField] private Transform groundCheckTransform = null;
+	[SerializeField] private LayerMask playerMask;
 
 	private bool jumpKeyWasPressed;
 	private float horizontalInput;
 	private Rigidbody rigidbodyComponent;
 	private bool isGrounded;
+	private int superJumpsRemaining;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		rigidbodyComponent = GetComponent<Rigidbody>();
 	}
 	
@@ -30,18 +32,26 @@ public class Player : MonoBehaviour {
 	//fixed update is called once every physics update
     private void FixedUpdate()
     {
-        if (!isGrounded)
+		rigidbodyComponent.velocity = new Vector3(horizontalInput * 2, rigidbodyComponent.velocity.y, 0); // take the horiztonal input to move back and forth, keep the vertical (y) same
+
+		if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0) // not colliding with anything
         {
-			return;
+			return; // then don't let it jump again until it's colliding again
         }
 
 		if (jumpKeyWasPressed)
 		{
-			rigidbodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+			float jumpPower = 5;
+			if (superJumpsRemaining > 0)
+            {
+				jumpPower *= 2;
+				superJumpsRemaining--;
+            }
+			rigidbodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
 			jumpKeyWasPressed = false;
 		}
 
-		rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+		
 	}
 
     private void OnCollisionEnter(Collision collision)
@@ -52,5 +62,18 @@ public class Player : MonoBehaviour {
     private void OnCollisionExit(Collision collision)
     {
 		isGrounded = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7) // 7 is the coin layer
+        {
+			Destroy(other.gameObject);
+        }
+		if (other.gameObject.layer == 8) // 8 is jumpcube
+        {
+			superJumpsRemaining++;
+			Destroy(other.gameObject);
+		}
     }
 }
